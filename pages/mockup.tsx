a7309e5082fcdf6645e8b7b358fca64ec638919b
fadhilahm <fadhilahmetra@gmail.com>
2022-10-27 12:06:28 +0900

    Added mockup components.

---
+++ pages/mockup.tsx
@@ -0,0 +1,15 @@
+import Codes from "../components/mockup/Codes";
+import Phones from "../components/mockup/Phones";
+import Windows from "../components/mockup/Windows";
+
+const Mockup = () => {
+  return (
+    <>
+      <Codes />
+      <Phones />
+      <Windows />
+    </>
+  );
+};
+
+export default Mockup;
